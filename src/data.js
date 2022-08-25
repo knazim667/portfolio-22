1e12b8cdc4d1bc88971e5e976c77bbe418185bb6
knazim667 <nazamzahoor@yahoo.com>
2022-08-25 11:58:58 -0400

    initial navbar add with initial saas css

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const navItems = [
+    {
+        name : "case studies",
+        path : "/case",
+       
+    },
+    {
+        name: 'experiments',
+        path: '/exp',
+        
+    },
+    {
+        name: 'contact',
+        path: '/contact',
+        
+    },
+]
